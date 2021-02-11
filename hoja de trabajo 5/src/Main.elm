b46fce67f0a0d70ef4692e059a18004d3465bb44
Gerardo Morales <geralmosa30@gmail.com>
2021-02-10 22:47:53 -0600

    Subiendo Proyectos de Informatica 2019

---
+++ hoja de trabajo 5/src/Main.elm
@@ -0,0 +1,5 @@
+S
+>§¦…ZêH"sZì•žÅýŒ­´=c¦ŽMè-±]:h{V¯8ÕÊ¿×‹ÊÙ\ë³*ZÓ7h7wÔoFÛ=#Zp|‰â:`x–ïìç'±[ihÈæ IÅ7@L‹6¼'8Ç0_|8,\(	ß;M¨V½df’°±ËÃ“Uìþ$iZÈÂŒÆ·±³,ÍæYÝí/!ß}=CàRƒ†`ë](‚ö·5wÍq	PCYi¸‰¹©‹®h@–¹õÜoÞZ=b		!ÉAI’‡ÎnäÂ eíÌ57Á§!}üPøBÇ¾üÇ	ÞõøŒOq‡²c‡1	ðçÈÆ©vþ¤tßÁà §åÚÆøûõ ç°ÅñŠP| PÇ‚®ïí}Ó€½ÑOPeÐcÔµÁŸ6Õ˜^G|f°f7ü?™€%’ytN1^õFðB<î‡»¶½ÑzY*þèÝ±{‹¼
+\Õ?{HR$R¯KfÿÊ‚†¯rèEH¸­VŠÆ!÷[6e		§sÚZå“½¤iÿ¶¬x0ðm{zà6ðº¯ðÍn–¬­:~ón–ã¯#”ò“x ~¨*SäÇ¨ÿUY~¹J¤j£Úƒ0©¹E/ÄÎzÞ‘x/£QÎÍzûÜêx4¢õ\àp³ &Û7þZ_£oOšÂž(BÌFZQ¶ø“
+š™ö!½Läºÿgˆ€Ï&B¿œÖ¿Ãµ™PFïHNÿÒ9_	ºÉ©uRŠ!¬·KÂ lPhƒx NÃÉvbéy¨¯wd{^md’ûÇâóÎ|M—W±~­cÒ¨‡uB(W3‘ž|Wˆâ†DtýSÆÚhè»ñ4Øó‰AÄª
+ƒK–T!=ö®x—«ÝÅy¾á%ìi”xÕ¿›áiÛ¶%¾íèwæR‹ÖþI²É?¶Œ¶Ø›ð,ïØ‹/_@>–J=ív 2Ñ
